7c0d327636e264e0ac4312d58196554ad85a8abb
Kiril Zolotarov <zolotarovkr@gmail.com>
2024-11-12 22:43:59 +0200

    Personal Details & Languages

---
+++ web/static/create/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('form', () => ({
+        personalDetails: {
+            fullName: '',
+            gender: '',
+            age: '',
+            citizenship: '',
+            maritalStatus: '',
+        },
+        contacts: {
+            emails: [],
+            phoneNumbers: [],
+            location: {
+                country: '',
+                city: '',
+            },
+        },
+        education: [],
+        workExperience: [],
+        languages: [],
+        programmingLanguages: [],
+
+        submit() {
+            console.log(this.languages);
+        },
+    }));
+});
