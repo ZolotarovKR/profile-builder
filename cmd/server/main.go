e612eec128575db9db0c0993ec5753892f5a4aab
Kiril Zolotarov <zolotarovkr@gmail.com>
2024-11-12 13:59:39 +0200

    Remove id pattern from handlers path

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,25 +13,25 @@ func main() {
 
 	r.HandleFunc("/api/previews", handlers.GetPreviews).Methods("GET")
 	r.HandleFunc("/api/cv", handlers.PostCv).Methods("POST")
-	r.HandleFunc("/api/cv/{id:[0-9]+}", handlers.DeleteCv).Methods("DELETE")
+	r.HandleFunc("/api/cv/{id}", handlers.DeleteCv).Methods("DELETE")
 
-	r.HandleFunc("/api/personal-details/{id:[0-9]+}", handlers.GetPersonalDetails).Methods("GET")
-	r.HandleFunc("/api/personal-details/{id:[0-9]+}", handlers.PutPersonalDetails).Methods("PUT")
+	r.HandleFunc("/api/personal-details/{id}", handlers.GetPersonalDetails).Methods("GET")
+	r.HandleFunc("/api/personal-details/{id}", handlers.PutPersonalDetails).Methods("PUT")
 
-	r.HandleFunc("/api/contacts/{id:[0-9]+}", handlers.GetContacts).Methods("GET")
-	r.HandleFunc("/api/contacts/{id:[0-9]+}", handlers.PutContacts).Methods("PUT")
+	r.HandleFunc("/api/contacts/{id}", handlers.GetContacts).Methods("GET")
+	r.HandleFunc("/api/contacts/{id}", handlers.PutContacts).Methods("PUT")
 
-	r.HandleFunc("/api/education/{id:[0-9]+}", handlers.GetEducation).Methods("GET")
-	r.HandleFunc("/api/education/{id:[0-9]+}", handlers.PutEducation).Methods("PUT")
+	r.HandleFunc("/api/education/{id}", handlers.GetEducation).Methods("GET")
+	r.HandleFunc("/api/education/{id}", handlers.PutEducation).Methods("PUT")
 
-	r.HandleFunc("/api/work-experience/{id:[0-9]+}", handlers.GetWorkExperience).Methods("GET")
-	r.HandleFunc("/api/work-experience/{id:[0-9]+}", handlers.PutWorkExperience).Methods("PUT")
+	r.HandleFunc("/api/work-experience/{id}", handlers.GetWorkExperience).Methods("GET")
+	r.HandleFunc("/api/work-experience/{id}", handlers.PutWorkExperience).Methods("PUT")
 
-	r.HandleFunc("/api/languages/{id:[0-9]+}", handlers.GetLanguages).Methods("GET")
-	r.HandleFunc("/api/languages/{id:[0-9]+}", handlers.PutLanguages).Methods("PUT")
+	r.HandleFunc("/api/languages/{id}", handlers.GetLanguages).Methods("GET")
+	r.HandleFunc("/api/languages/{id}", handlers.PutLanguages).Methods("PUT")
 
-	r.HandleFunc("/api/programming-languages/{id:[0-9]+}", handlers.GetProgrammingLanguages).Methods("GET")
-	r.HandleFunc("/api/programming-languages/{id:[0-9]+}", handlers.PutProgrammingLanguages).Methods("PUT")
+	r.HandleFunc("/api/programming-languages/{id}", handlers.GetProgrammingLanguages).Methods("GET")
+	r.HandleFunc("/api/programming-languages/{id}", handlers.PutProgrammingLanguages).Methods("PUT")
 
 	log.Print("Starting server at :8080")
 	err := http.ListenAndServe(":8080", r)
