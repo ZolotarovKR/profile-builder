c80def4f5e9273b9a904787ece4071fe89bfdfbc
Kiril Zolotarov <zolotarovkr@gmail.com>
2024-11-13 19:10:28 +0200

    Implement form submitting

--- web/static/create/script.js
+++ web/static/create/script.js
@@ -3,7 +3,7 @@ document.addEventListener('alpine:init', () => {
         personalDetails: {
             fullName: '',
             gender: '',
-            age: '',
+            age: 18,
             citizenship: '',
             maritalStatus: '',
         },
@@ -20,8 +20,29 @@ document.addEventListener('alpine:init', () => {
         languages: [],
         programmingLanguages: [],
 
-        submit() {
-            console.log(this.workExperience);
+        async submit() {
+            const data = {};
+            data.personalDetails = Alpine.raw(this.personalDetails);
+            data.contacts = Alpine.raw(this.contacts);
+            data.education = Alpine.raw(this.education);
+            data.workExperience = Alpine.raw(this.workExperience);
+            data.languages = Alpine.raw(this.languages);
+            data.programmingLanguages = Alpine.raw(this.programmingLanguages);
+            let resp = await fetch('/api/cv', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            switch (resp.status) {
+                case 201:
+                    alert('Created');
+                    break;
+                case 400:
+                    alert(await resp.text());
+                    break;
+            }
         },
     }));
 });
