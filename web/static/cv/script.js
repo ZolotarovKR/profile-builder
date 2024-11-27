9f579926cc9d4cfec1aa6927579006482ffc24bb
Kiril Zolotarov <zolotarovkr@gmail.com>
2024-11-27 23:31:42 +0200

    Major changes

--- web/static/cv/script.js
+++ web/static/cv/script.js
@@ -1,194 +1,184 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('cv', () => ({
-        id: 0,
-        activeSection: 'personalDetails',
-        relevantSections: [],
-        inEditMode: false,
-
-        personalDetails: {
-            fullName: '',
-            gender: '',
-            age: '',
-            citizenship: '',
-            maritalStatus: '',
+    Alpine.store('id', new URLSearchParams(window.location.search).get('id'));
+    Alpine.store('activeSection', 'personalDetails');
+    Alpine.store('inEditMode', false);
+    Alpine.store('deleteCv', async () => {
+        if (!confirm('Are you sure?')) return;
+        await fetch('/api/cv/' + Alpine.store('id'), {
+            method: 'DELETE',
+        });
+        window.location.href = '/';
+    });
+    Alpine.data('personalDetails', () => ({
+        isUpToDate: false,
+        fullName: '',
+        gender: '',
+        age: '',
+        citizenship: '',
+        maritalStatus: '',
+        async get() {
+            if (this.isUpToDate) return;
+            const resp = await fetch('/api/personal-details/' + Alpine.store('id'));
+            Object.assign(this, await resp.json());
+            this.isUpToDate = true;
         },
-        contacts: {
-            emails: [],
-            phoneNumbers: [],
-            location: {
-                country: '',
-                city: '',
-            },
-        },
-        education: [],
-        workExperience: [],
-        languages: [],
-        programmingLanguages: [],
-
-        pd: {
-            fullName: '',
-            gender: '',
-            age: '',
-            citizenship: '',
-            maritalStatus: '',
-        },
-        c: {
-            emails: [],
-            phoneNumbers: [],
-            location: {
-                country: '',
-                city: '',
-            },
-        },
-        e: [],
-        we: [],
-        ls: [],
-        pls: [],
-
-        async init() {
-            let params = new URLSearchParams(window.location.search);
-            this.id = Number(params.get('id'));
-            await this.fetchActiveSection();
-        },
-        enterEditMode() {
-            switch (this.activeSection) {
-                case 'personalDetails':
-                    this.pd = structuredClone(Alpine.raw(this.personalDetails));
-                    break;
-                case 'contacts':
-                    this.c = structuredClone(Alpine.raw(this.contacts));
-                    break;
-                case 'education':
-                    this.e = structuredClone(Alpine.raw(this.education));
-                    break;
-                case 'workExperience':
-                    this.we = structuredClone(Alpine.raw(this.workExperience));
-                    break;
-                case 'languages':
-                    this.ls = structuredClone(Alpine.raw(this.languages));
-                    break;
-                case 'programmingLanguages':
-                    this.pls = structuredClone(Alpine.raw(this.programmingLanguages));
-                    break;
-                default:
-                    return;
-            }
-            this.inEditMode = true;
-        },
-        async selectSection(s) {
-            if (this.inEditMode) return;
-
-            this.activeSection = s;
-            await this.fetchActiveSection();
-        },
-        async fetchActiveSection() {
-            if (this.relevantSections.includes(this.activeSection)) return;
-
-            let url = this.getActiveSectionUrl();
-
-            let resp = await fetch(url);
-            if (resp.status !== 200) {
-                alert(await resp.text());
-                return;
-            }
-            let data = await resp.json();
-            switch (this.activeSection) {
-                case 'personalDetails':
-                    this.personalDetails = data;
-                    break;
-                case 'contacts':
-                    this.contacts = data;
-                    break;
-                case 'education':
-                    this.education = data;
-                    break;
-                case 'workExperience':
-                    this.workExperience = data;
-                    break;
-                case 'languages':
-                    this.languages = data;
-                    break;
-                case 'programmingLanguages':
-                    this.programmingLanguages = data;
-                    break;
-                default:
-                    return;
-            }
-            this.relevantSections.push(this.activeSection);
-        },
-        async saveActiveSection() {
-            let url = this.getActiveSectionUrl();
-            let data;
-            switch (this.activeSection) {
-                case 'personalDetails':
-                    data = structuredClone(Alpine.raw(this.pd));
-                    data.age = Number(data.age);
-                    break;
-                case 'contacts':
-                    data = structuredClone(Alpine.raw(this.c));
-                    break;
-                case 'education':
-                    data = structuredClone(Alpine.raw(this.e));
-                    break;
-                case 'workExperience':
-                    data = structuredClone(Alpine.raw(this.we));
-                    break;
-                case 'languages':
-                    data = structuredClone(Alpine.raw(this.ls));
-                    break;
-                case 'programmingLanguages':
-                    data = structuredClone(Alpine.raw(this.pls));
-                    break;
-                default:
-                    console.error('Unknown section: ' + s);
-                    return;
-            }
-
-            let resp = await fetch(url, {
+    }));
+    Alpine.data('personalDetailsForm', () => ({
+        fullName: '',
+        gender: '',
+        age: '',
+        citizenship: '',
+        maritalStatus: '',
+        async save() {
+            const data = {};
+            data.fullName = this.fullName;
+            data.gender = this.gender;
+            data.age = Number(this.age);
+            data.citizenship = this.citizenship;
+            data.maritalStatus = this.maritalStatus;
+            const resp = await fetch('/api/personal-details/' + Alpine.store('id'), {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(data),
             });
-            if (resp.status !== 200) {
-                alert(await resp.text());
-                return;
-            }
-            this.relevantSections.splice(this.relevantSections.indexOf(this.activeSection), 1);
-            await this.fetchActiveSection();
-            this.inEditMode = false;
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
         },
-        async deleteThisCV() {
-            if (!confirm('Are you sure?')) return;
-
-            let resp = await fetch('/api/cv/' + this.id, {
-                method: 'DELETE',
+    }));
+    Alpine.data('contacts', () => ({
+        isUpToDate: false,
+        emails: [],
+        phoneNumbers: [],
+        location: {
+            country: '',
+            city: '',
+        },
+        async get() {
+            if (this.isUpToDate) return;
+            const resp = await fetch('/api/contacts/' + Alpine.store('id'));
+            Object.assign(this, await resp.json());
+            this.isUpToDate = true;
+        },
+    }));
+    Alpine.data('contactsForm', () => ({
+        emails: [],
+        phoneNumbers: [],
+        location: {
+            country: '',
+            city: '',
+        },
+        async save() {
+            const data = {};
+            data.emails = this.emails;
+            data.phoneNumbers = this.phoneNumbers;
+            data.location = this.location;
+            const resp = await fetch('/api/contacts/' + Alpine.store('id'), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
             });
-            if (resp.status !== 200) {
-                alert(await resp.text());
-                return;
-            }
-            window.location.href = '/';
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
         },
-        getActiveSectionUrl() {
-            switch (this.activeSection) {
-                case 'personalDetails':
-                    return '/api/personal-details/' + this.id;
-                case 'contacts':
-                    return '/api/contacts/' + this.id;
-                case 'education':
-                    return '/api/education/' + this.id;
-                case 'workExperience':
-                    return '/api/work-experience/' + this.id;
-                case 'languages':
-                    return '/api/languages/' + this.id;
-                case 'programmingLanguages':
-                    return '/api/programming-languages/' + this.id;
-                default:
-                    console.error('Unknown section: ' + s);
-                    return null;
-            }
+    }));
+    Alpine.data('education', () => ({
+        isUpToDate: false,
+        courses: [],
+        async get() {
+            const resp = await fetch('/api/education/' + Alpine.store('id'));
+            this.courses = await resp.json();
+            this.isUpToDate = true;
+        },
+    }));
+    Alpine.data('educationForm', () => ({
+        courses: [],
+        async save() {
+            const data = this.courses;
+            const resp = await fetch('/api/education/' + Alpine.store('id'), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
+        },
+    }));
+    Alpine.data('workExperience', () => ({
+        isUpToDate: false,
+        jobRecords: [],
+        async get() {
+            const resp = await fetch('/api/work-experience/' + Alpine.store('id'));
+            this.jobRecords = await resp.json();
+            this.isUpToDate = true;
+        },
+    }));
+    Alpine.data('workExperienceForm', () => ({
+        jobRecords: [],
+        async save() {
+            const data = this.jobRecords;
+            const resp = await fetch('/api/work-experience/' + Alpine.store('id'), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
+        },
+    }));
+    Alpine.data('languages', () => ({
+        isUpToDate: false,
+        languages: [],
+        async get() {
+            const resp = await fetch('/api/languages/' + Alpine.store('id'));
+            this.languages = await resp.json();
+            this.isUpToDate = true;
+        },
+    }));
+    Alpine.data('languagesForm', () => ({
+        languages: [],
+        async save() {
+            const data = this.languages;
+            const resp = await fetch('/api/languages/' + Alpine.store('id'), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
+        },
+    }));
+    Alpine.data('programmingLanguages', () => ({
+        isUpToDate: false,
+        programmingLanguages: [],
+        async get() {
+            const resp = await fetch('/api/programming-languages/' + Alpine.store('id'));
+            this.programmingLanguages = await resp.json();
+            this.isUpToDate = true;
+        },
+    }));
+    Alpine.data('programmingLanguagesForm', () => ({
+        programmingLanguages: [],
+        async save() {
+            const data = this.programmingLanguages;
+            const resp = await fetch('/api/programming-languages/' + Alpine.store('id'), {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            if (resp.status === 400) alert(await resp.text());
+            return resp.status;
         },
     }));
 });
